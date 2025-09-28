d5eddae2de3aea45db0052c0454c33efa2ffe6d6
Amjad-Alawad <amjadalawad5@gmail.com>
2025-09-28 21:32:01 +0300

    Update Procedure tablo.sql

--- Procedure tablo.sql
+++ Procedure tablo.sql
@@ -18,12 +18,16 @@ Delete Urunler where UrunID = @UrunID
 end
 
 
-Create procedure Urun_Arama
-@UrunID int
-as 
-begin
-Select * from Urunler where UrunID = @UrunID
-end
+create PROCEDURE Urun_Arama
+    @UrunID INT
+AS
+BEGIN
+    SELECT u.UrunID, u.UrunAdi, u.BirimFiyat, t.TedarikciAdi, k.KategoriAdi
+    FROM Urunler u
+    JOIN Tedarikciler t ON u.TedarikciID = t.TedarikciID
+    JOIN Kategoriler k ON u.KategoriID = k.KategoriID
+    WHERE u.UrunID = @UrunID
+END
 
 
 CREATE PROCEDURE Kayıt_Guncelle
